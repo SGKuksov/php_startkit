58c24acc6c7f6f73d54bc0e12d9272c152ee240a
Kuksov Sergei <sgkuksov1989@gmail.com>
2021-09-12 14:06:30 +0300

    Fix packages. Add mysql connection

--- web/public/index.php
+++ web/public/index.php
@@ -5,3 +5,15 @@ use Otus\Lessons\Lesson4\Example;
 
 $example = new Example;
 echo $example->getName() . PHP_EOL;
+
+$username = "root";
+$password = "root";
+
+try {
+    $conn = new PDO('mysql:host=host.docker.internal;port=8989;dbname=test_db', $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
